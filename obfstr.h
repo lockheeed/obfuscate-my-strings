1f463368d583d80e57e8e177d0807d84e835ec71
lockheeed <anonimus140119@gmail.com>
2023-08-01 18:36:12 +0300

    obfstr.h

---
+++ obfstr.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <stddef.h>
+
+
+namespace obfstr
+{
+    constexpr char key[] = { 0xae, 0x60, 0x53, 0xff, 0xde, 0xda };
+
+    template<size_t size>
+    struct MetaString
+    {
+        char buff[size];
+
+        constexpr MetaString() : buff{ 0 } {}
+
+        char* deobfuscate()
+        {
+            for (size_t i = 0; i < size - 1; i++) 
+                buff[i] ^= key[i % sizeof(key)];
+            return buff;
+        }
+    };
+
+    template<size_t size>
+    constexpr auto obfuscate(const char plaintext[size])
+    {
+        MetaString<size> obfuscated;
+
+        for (size_t i = 0; i < size - 1; i++)
+            obfuscated.buff[i] = plaintext[i] ^ key[i % sizeof(key)];
+
+        obfuscated.buff[size - 1] = 0;
+
+        return obfuscated;
+    }
+}
+
+
+#define OBFSTR(x) []() { constexpr auto encoded = obfstr::obfuscate<sizeof(x)>(x); return encoded; }().deobfuscate()
